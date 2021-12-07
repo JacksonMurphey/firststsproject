7f3077767b6df592ebbae3fe45263ddcce84016b
JacksonMurphey <jmurphey88@gmail.com>
2021-12-06 19:54:46 -0600

    first commit

---
+++ src/test/java/com/jmurphey/firststring/FirstStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmurphey.firststring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
